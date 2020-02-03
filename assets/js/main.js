8e652de03b07ded5ba10f0c3d6a028530c4647be
Greugreu <fenris.fitz@hotmail.fr>
2020-02-03 14:32:27 +0100

    Le commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,35 @@
 //FLEXSLIDER
-$(document).ready(function() {
-	$('flexslider').flexslider({
-		animation: "slide",
-		directionNav: false,
+$( document ).ready(function() {
+	$(".flexslider")
+		.flexslider({
+			animation: "slide",
+			smoothHeight: true,
+			controlNav: false,
+			directionNav: false
+		});
+
+	$(".flexslider.slider")
+		.flexslider({
+			animation: "slide",
+			smoothHeight: true,
+			directionNav: true,
+			controlNav: true
+		});
+
+	$( ".cross" ).hide();
+	$( ".menu" ).hide();
+	$( ".hamburger" ).click(function() {
+		$( ".menu" ).slideToggle( "slow", function() {
+			$( ".hamburger" ).hide();
+			$( ".cross" ).show();
+		});
 	});
+
+	$( ".cross" ).click(function() {
+		$( ".menu" ).slideToggle( "slow", function() {
+			$( ".cross" ).hide();
+			$( ".hamburger" ).show();
+		});
+	});
+
 });
